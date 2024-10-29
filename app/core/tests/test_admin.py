f495c7fdfe55287dd6f61346f78343c6a3f0d9c4
vishnu <dasv2017@gmail.com>
2024-10-29 22:39:54 +0530

    completed supporting modifying users

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,34 @@
+"""
+Tests for the DJango admin modifications
+"""
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='vishnu@gmail.com',
+            password='123',
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpass123',
+            name='Test User'
+        )
+    
+    def test_users_list(self):
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+        
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+        
+        self.assertEqual(res.status_code, 200)
